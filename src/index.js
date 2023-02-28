e3b3d75dbeab5b99261913a67d7311aab3c157cc
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-02-28 16:13:48 +0800

    updated version

--- src/index.js
+++ src/index.js
@@ -46,9 +46,45 @@ document.addEventListener('DOMContentLoaded', () => {
         reader.readAsArrayBuffer(files[0]);
   }
 
-
   function displayData(jsonData) {
-    for (let i = 0; i < jsonData.length; i++) {
-      console.log(jsonData[i].no);
+    const table = document.getElementById('table');
+    const numRows = jsonData.length;
+
+    for (let i = 0; i < numRows; i++) {
+      const row = table.insertRow();
+      const no = row.insertCell();
+      const id = row.insertCell();
+      const name = row.insertCell();
+      const joined = row.insertCell();
+      const section = row.insertCell();
+      const line = row.insertCell();
+      const resigned = row.insertCell();
+
+      no.textContent = jsonData[i].no;
+      id.textContent = jsonData[i]["id opt"];
+      name.textContent = jsonData[i].name;
+      joined.textContent = jsonData[i]["joined date"];
+      section.textContent = jsonData[i].section;
+      line.textContent = jsonData[i].line;
+      resigned.textContent = jsonData[i]["resigned date"];
+    }
+  }
+
+  function searchData() {
+    const input = document.getElementById('search');
+    const filter = input.value.toUpperCase();
+    const table = document.getElementById('table');
+    const tr = table.getElementsByTagName('tr');
+
+    for (let i = 0; i < tr.length; i++) {
+      const td = tr[i].getElementsByTagName('td')[0];
+      if (td) {
+        const textValue = td.textContent || td.innerText;
+        if (textValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = '';
+        } else {
+          tr[i].style.display = 'none';
+        }
+      }
     }
   }
